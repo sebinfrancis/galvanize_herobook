427f6272d1efbb8b5033c4b91f18003015283f60
sebin <sebin.francis@cognizant.com>
2021-03-15 18:55:06 -0400

    added repo

---
+++ src/main/java/com/example/herobook/HerobookApplication.java
@@ -0,0 +1,13 @@
+package com.example.herobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerobookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/herobook/HerobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
