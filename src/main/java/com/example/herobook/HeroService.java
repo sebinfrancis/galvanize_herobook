6ba9778efae51b4084130a56432736eaa0ec81ab
sebin <sebin.francis@cognizant.com>
2021-03-16 19:35:11 -0400

    added heroservice fetchall

--- src/main/java/com/example/herobook/HeroService.java
+++ src/main/java/com/example/herobook/HeroService.java
@@ -2,6 +2,10 @@ package com.example.herobook;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class HeroService {
     private final HeroBookRepository repository;
@@ -15,5 +19,14 @@ public class HeroService {
                 new HeroEntity(heroDto.getName(),heroDto.getSuper_power() )
         );
 
+    }
+
+    public List<HeroDto> fetchAll() {
+        return repository.findAll()
+                .stream()
+                .map(heroEntity -> {
+                    return new HeroDto(heroEntity.getName(), heroEntity.getSuper_power());
+                })
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/example/herobook/HeroServiceTest.java
+++ src/test/java/com/example/herobook/HeroServiceTest.java
@@ -6,7 +6,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class HeroServiceTest {
@@ -24,6 +28,23 @@ public class HeroServiceTest {
     verify(mockRepository).save(
             new HeroEntity("Superman", "Flying")
     );
+    }
 
-}
+    @Test
+    void fetchAllTest() {
+        when(mockRepository.findAll()).thenReturn(
+                List.of(
+                        new HeroEntity("Superman", "Flying"),
+                        new HeroEntity("Batman", "Jumping")
+        ));
+
+        List<HeroDto> heroDtos =subject.fetchAll();
+
+        assertThat(heroDtos).isEqualTo(
+                List.of(
+                        new HeroDto("Superman", "Flying"),
+                        new HeroDto("Batman", "Jumping")
+                )
+        );
+    }
 }
